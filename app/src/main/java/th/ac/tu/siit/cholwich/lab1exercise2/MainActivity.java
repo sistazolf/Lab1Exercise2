4f648d9c6f7f5de4cad3053ce6acdb65c019a813
reangreyro <reangreyro@gmail.com>
2015-01-23 15:08:40 +0700

    Lab 1 - Exercise 2 - Temperature calculator

--- app/src/main/java/th/ac/tu/siit/cholwich/lab1exercise2/MainActivity.java
+++ app/src/main/java/th/ac/tu/siit/cholwich/lab1exercise2/MainActivity.java
@@ -4,15 +4,83 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public void convert(View v) {
+        EditText etInput = (EditText) findViewById(R.id.etInput);
+        String s = etInput.getText().toString();
+        double val = Double.parseDouble(s);
+
+
+    RadioGroup rgFrom = (RadioGroup) findViewById(R.id.rgFrom);
+    int selFrom = rgFrom.getCheckedRadioButtonId();
+
+      if(selFrom==R.id.rbFrmC){ //The user wants to convert from Celsius.
+        RadioGroup rgTo = (RadioGroup) findViewById(R.id.rgTo);
+        int selTo = rgTo.getCheckedRadioButtonId();
+
+        TextView tv = (TextView) findViewById(R.id.tvOutput);
+
+        if (selTo == R.id.rbToF) {
+            double CtoF = ((val * 9/5) + 32);
+            tv.setText(Double.toString(CtoF));
+        }else if (selTo == R.id.rbToK) {
+            double CtoK = val + 273.15;
+            tv.setText(Double.toString(CtoK));
+        }else{
+            tv.setText(Double.toString(val));
+        }
+      }
+
+        if(selFrom==R.id.rbFrmF){ //The user wants to convert from Celsius.
+            RadioGroup rgTo = (RadioGroup) findViewById(R.id.rgTo);
+            int selTo = rgTo.getCheckedRadioButtonId();
+
+            TextView tv = (TextView) findViewById(R.id.tvOutput);
+
+            if (selTo == R.id.rbToC) {
+                double FtoC = ((val-32) * 5/9);
+                tv.setText(Double.toString(FtoC));
+            }else if (selTo == R.id.rbToK) {
+                double FtoK = (val + 459.67) * 5/9;
+                tv.setText(Double.toString(FtoK));
+            }else{
+                tv.setText(Double.toString(val));
+            }
+        }
+
+        if(selFrom==R.id.rbFrmK){ //The user wants to convert from Celsius.
+            RadioGroup rgTo = (RadioGroup) findViewById(R.id.rgTo);
+            int selTo = rgTo.getCheckedRadioButtonId();
+
+            TextView tv = (TextView) findViewById(R.id.tvOutput);
+
+            if (selTo == R.id.rbToC) {
+                double KtoC = (val-273.15);
+                tv.setText(Double.toString(KtoC));
+            }else if (selTo == R.id.rbToF) {
+                double KtoF = (val*9/5) - 459.67;
+                tv.setText(Double.toString(KtoF));
+            }else{
+                tv.setText(Double.toString(val));
+            }
+        }
+    }
+
 
 
     @Override
